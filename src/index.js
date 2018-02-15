d0afbd7e8cf2bc7622f57b2f2656c5c2ac79b947
Ryan Dabler <rpdabler@gmail.com>
2018-02-15 12:47:37 -0700

    replace generic <App> with <Board>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Board from './components/board';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Board />, document.getElementById('root'));
