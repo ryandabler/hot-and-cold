6c3bdeb20150e0880b318b4219097becb6929219
Ryan Dabler <rpdabler@gmail.com>
2018-02-16 08:02:37 -0700

    Hook up guessing functionality

--- src/components/game.js
+++ src/components/game.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import './header.css';
+import GuessForm from "./guess-form";
+import GuessHistory from "./guess-history";
 
-export default function Game() {
-    return (
-        <div>
-        </div>
-    );
+import './game.css';
+
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [],
+            answer: 25
+        }
+    }
+
+    addGuess(guess) {
+        const history = [...this.state.history, guess];
+        this.setState({
+            history
+        });
+        console.log(guess);
+    }
+
+    render() {
+        return (
+            <div>
+                <GuessForm onSubmit={guess => this.addGuess(guess)} />
+                <div>Guesses: {this.state.history.length}</div>
+                <GuessHistory history={this.state.history}/>
+            </div>
+        );
+    }
 }
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './guess-form.css';
 
-export default function GuessForm() {
-    return (
-        <form>
-            <input type="text" />
-            <input type="submit" value="Guess"/>
-        </form>
-    );
+export default class GuessForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.textInput.value);
+    }
+
+    render() {
+        return (
+            <form onSubmit={event => this.onSubmit(event)}>
+                <input ref={input => this.textInput = input} type="text" />
+                <input type="submit" value="Guess" />
+            </form>
+        );
+    }
 }
